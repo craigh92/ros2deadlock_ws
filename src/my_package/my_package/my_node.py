8b460d5ea5edbc96e0d430f0e4987e8731a30693
Craig Hickman <craig.hickman@ukaea.uk>
2020-06-19 16:26:12 +0100

    Print thread ID in callbacks and logging messages

--- src/my_package/my_package/my_node.py
+++ src/my_package/my_package/my_node.py
@@ -4,8 +4,10 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from rclpy.task import Future
+from rclpy.waitable import Waitable
 import os
 import argparse
+import threading
 
 latching_qos = QoSProfile(depth=1,
     durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
@@ -22,7 +24,10 @@ def main():
 
     args = parser.parse_args()
 
+    # Init ros
+
     rclpy.init()
+    exe = MultiThreadedExecutor() if args.exe == 'M' else SingleThreadedExecutor()
 
     # Set up publisher
 
@@ -31,7 +36,7 @@ def main():
     pub1 = pubnode.create_publisher(String, 'topic1', latching_qos)
     msg1 = String()
     msg1.data = "hello1"
-    pubnode.get_logger().info("Publishing hello1")
+    pubnode.get_logger().info("Publishing hello1: Thread {}".format(threading.current_thread().name))
     pub1.publish(msg1)
 
 
@@ -41,22 +46,22 @@ def main():
 
     subnode = Node('subnode_' + str(os.getpid()))
     subnode.create_subscription(String, 'topic1', lambda msg : ([
-            subnode.get_logger().info("Received message on topic1"),
-            future_msgs.set_result(msg)
+
+            subnode.get_logger().info("Received message on topic1: Thread {}".format(threading.current_thread().name)),
+            future_msgs.set_result(msg),
+            subnode.get_logger().info("Callback finished")
+
     ]), latching_qos)
 
     # Start nodes
 
-    exe = MultiThreadedExecutor() if args.exe == 'M' else SingleThreadedExecutor()
     print("Using {}".format(str(type(exe))))
     exe.add_node(pubnode)
     exe.add_node(subnode)
-
-    future_msgs.add_done_callback(lambda fut : print("Future is done"))
     
     if args.d:
         while future_msgs.done() is False:
-            print('------ Begin Spin')
+            print('------ Begin Spin : {}'.format(threading.current_thread().name))
             exe.spin_once()
             print('------ End Spin')
     else:
